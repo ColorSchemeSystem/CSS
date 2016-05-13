ac3e87f9756c2985461a0dfd1211060f40828ea2
yuji.momoi <yuji.momoi@Apple-no-MacBook-Pro-5.local>
2016-05-13 17:58:55 +0900

    実験中

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,13 +2,24 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.data.Form;
 
 import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
+	public static Result index() {
+		String uuid=session("uuid");
+		if(uuid==null) {
+			uuid=java.util.UUID.randomUUID().toString();
+			session("uuid", uuid);
+		}
+		return ok(index.render("UserID:" + uuid));
+	}
 
+	public static Result submit() {
+		String name = Form.form().bindFromRequest().get("name");
+		String myId = session("uuid");
+		return ok(index.render("リクエスト受け取った！貴様の名前は→" + name +"だな！？" + "セッションIDは:" + myId + "だッッ！"));
+	}
 }
---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,67 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table ColorSchemes (
+  color_scheme_id           bigint not null,
+  create_ts                 date,
+  update_ts                 date,
+  versions                  bigint,
+  template_id               varchar(255),
+  class_name                varchar(255),
+  color_hex                 varchar(255),
+  display_name              varchar(255),
+  constraint pk_ColorSchemes primary key (color_scheme_id))
+;
+
+create table Members (
+  member_id                 bigint not null,
+  create_ts                 date,
+  update_ts                 date,
+  versions                  bigint,
+  member_name               varchar(255),
+  password                  varchar(255),
+  mail                      varchar(255),
+  constraint pk_Members primary key (member_id))
+;
+
+create table Templates (
+  template_id               bigint not null,
+  create_ts                 date,
+  update_ts                 date,
+  versions                  bigint,
+  member_id                 bigint,
+  template_name             varchar(255),
+  message                   varchar(255),
+  access_flag               integer,
+  constraint pk_Templates primary key (template_id))
+;
+
+create sequence ColorSchemes_seq;
+
+create sequence Members_seq;
+
+create sequence Templates_seq;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists ColorSchemes;
+
+drop table if exists Members;
+
+drop table if exists Templates;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists ColorSchemes_seq;
+
+drop sequence if exists Members_seq;
+
+drop sequence if exists Templates_seq;
+
