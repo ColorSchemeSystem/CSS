71975967101aedba2295ce4dcfef8edc84702a96
ryuya.umino <r.umino@tcmobile.jp>
2016-05-31 11:24:20 +0900

    仮のHTMLのテンプレート化

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -26,9 +26,24 @@ create table Members (
   constraint pk_Members primary key (member_id))
 ;
 
+create table Templates (
+  template_id               bigint not null,
+  create_ts                 date,
+  update_ts                 date,
+  versions                  bigint,
+  member_id                 bigint,
+  template_name             varchar(255),
+  message                   varchar(255),
+  html                      varchar(255),
+  access_flag               integer,
+  constraint pk_Templates primary key (template_id))
+;
+
 create sequence ColorSchemes_seq;
 
 create sequence Members_seq;
+
+create sequence Templates_seq;
 
 
 
@@ -41,9 +56,13 @@ drop table if exists ColorSchemes;
 
 drop table if exists Members;
 
+drop table if exists Templates;
+
 SET REFERENTIAL_INTEGRITY TRUE;
 
 drop sequence if exists ColorSchemes_seq;
 
 drop sequence if exists Members_seq;
+
+drop sequence if exists Templates_seq;
 
