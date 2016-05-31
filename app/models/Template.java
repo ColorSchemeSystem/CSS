017b22d3058b590db3f61f3c746a482f37e5b189
ryuya.umino <r.umino@tcmobile.jp>
2016-05-31 15:56:25 +0900

    AppServiceの追加

--- app/models/Template.java
+++ app/models/Template.java
@@ -43,39 +43,7 @@ public class Template extends BaseModel {
 	 */
 	public static Template findById(Long templateId) {
 		return find.byId(templateId);
+
 	}
 
-	/**
-	 *
-	 * @param html
-	 * @return
-	 */
-	public static List<String> extractClasses(String html) {
-		Document document = Jsoup.parse(html);
-		Elements elements = document.body().children().select("*");
-		Map<String, Integer> classesMap = new HashMap<String, Integer>();
-		for (Element e : elements) {
-			String classValue = e.attr("class");
-			for (String c : classValue.split(" ")) {
-				if (classesMap.containsKey(c)) {
-					classesMap.put(c, classesMap.get(c) + 1);
-				} else {
-					classesMap.put(c, 1);
-				}
-			}
-		}
-		List<Map.Entry> entries = new ArrayList<Map.Entry>(classesMap.entrySet());
-		Collections.sort(entries, new Comparator() {
-			public int compare(Object o1, Object o2) {
-				Map.Entry e1 = (Map.Entry) o1;
-				Map.Entry e2 = (Map.Entry) o2;
-				return ((Integer) e2.getValue()).compareTo((Integer) e1.getValue());
-			}
-		});
-		List<String> classes = new ArrayList<String>();
-		for (Map.Entry entry : entries) {
-			classes.add((String) entry.getKey() + " : " + entry.getValue());
-		}
-		return classes;
-	}
 }
