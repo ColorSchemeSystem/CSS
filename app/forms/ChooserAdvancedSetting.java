495b6097e26ef58ce4329aa30144da81bc66bdf4
yuji.momoi <yuji.momoi@Apple-no-MacBook-Pro-5.local>
2016-06-03 13:14:34 +0900

    退避

---
+++ app/forms/ChooserAdvancedSetting.java
@@ -0,0 +1,11 @@
+package forms;
+
+import java.util.List;
+
+public class ChooserAdvancedSetting {
+	public Boolean hsvpanel;
+	public Boolean slider;
+	public Boolean swatche;
+
+	List<String> listSwatches;
+}
