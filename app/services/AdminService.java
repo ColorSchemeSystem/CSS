034d5d8d8a29c592ef3bbb5ae18ad0f7af61f525
masataka.okudera <m.okudera@tcmobile.jp>
2016-06-06 11:48:02 +0900

    会員系のメソッドをAdminControllerに移動

--- app/services/AdminService.java
+++ app/services/AdminService.java
@@ -1,9 +1,66 @@
 package services;
 
+import org.mindrot.jbcrypt.BCrypt;
+
+import models.Member;
 import models.Template;
 
 public class AdminService {
+	/**
+	 * 
+	 * @param template
+	 */
 	public void saveTemplate(Template template) {
 		template.save();
+	}
+	
+	/**
+	 * 
+	 * @param memberId
+	 * @return
+	 */
+	public Member findMemberById(Long memberId) {
+		return Member.find.byId(memberId);
+	}
+	
+	/**
+	 * 
+	 * @param memberName
+	 * @return
+	 */
+	public boolean memberExists(String memberName) {
+		Member member = Member.find.where().eq("memberName", memberName).findUnique();
+		if(member != null) {
+			return true;
+		}	else	{
+			return false;
+		}
+	}
+	
+	/**
+	 * 
+	 * @param memberName
+	 * @return
+	 */
+	public Member findMemberByMemberName(String memberName) {
+		Member member = Member.find.where().eq("memberName", memberName).findUnique();
+		return member;
+	}
+	
+	/**
+	 * 
+	 * @param member
+	 */
+	public void storeMember(Member member) {
+		member.save();
+	}
+	
+	/**
+	 * 
+	 * @param password
+	 * @return
+	 */
+	public String passwordHash(String password) {
+		return BCrypt.hashpw(password, BCrypt.gensalt());
 	}
 }
--- build.sbt
+++ build.sbt
@@ -9,7 +9,8 @@ libraryDependencies ++= Seq(
   "org.jsoup" % "jsoup" % "1.9.1",
   "commons-io" % "commons-io" % "2.5",
   "net.sf.flexjson" % "flexjson" % "3.3",
-  "commons-codec" % "commons-codec" % "1.10"
+  "commons-codec" % "commons-codec" % "1.10",
+  "org.mindrot" % "jbcrypt" % "0.3m"
 )
 
 play.Project.playJavaSettings
