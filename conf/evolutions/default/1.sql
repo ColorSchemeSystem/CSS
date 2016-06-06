2bd716dce6c6b936e492758223c9329273940d0b
yuji.momoi <yuji.momoi@Apple-no-MacBook-Pro-5.local>
2016-06-06 18:23:48 +0900

    2016/06/06作業終了

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -27,6 +27,17 @@ create table ColorSchemes (
   constraint pk_ColorSchemes primary key (color_scheme_id))
 ;
 
+create table Images (
+  image_id                  bigint not null,
+  create_ts                 date,
+  update_ts                 date,
+  versions                  bigint,
+  member_member_id          bigint,
+  image_name                varchar(255),
+  image_message             varchar(255),
+  constraint pk_Images primary key (image_id))
+;
+
 create table Members (
   member_id                 bigint not null,
   create_ts                 date,
@@ -56,14 +67,18 @@ create sequence choosers_seq;
 
 create sequence ColorSchemes_seq;
 
+create sequence Images_seq;
+
 create sequence Members_seq;
 
 create sequence Templates_seq;
 
-alter table Members add constraint fk_Members_chooser_1 foreign key (chooser_chooser_id) references choosers (chooser_id) on delete restrict on update restrict;
-create index ix_Members_chooser_1 on Members (chooser_chooser_id);
-alter table Templates add constraint fk_Templates_member_2 foreign key (member_member_id) references Members (member_id) on delete restrict on update restrict;
-create index ix_Templates_member_2 on Templates (member_member_id);
+alter table Images add constraint fk_Images_member_1 foreign key (member_member_id) references Members (member_id) on delete restrict on update restrict;
+create index ix_Images_member_1 on Images (member_member_id);
+alter table Members add constraint fk_Members_chooser_2 foreign key (chooser_chooser_id) references choosers (chooser_id) on delete restrict on update restrict;
+create index ix_Members_chooser_2 on Members (chooser_chooser_id);
+alter table Templates add constraint fk_Templates_member_3 foreign key (member_member_id) references Members (member_id) on delete restrict on update restrict;
+create index ix_Templates_member_3 on Templates (member_member_id);
 
 
 
@@ -75,6 +90,8 @@ drop table if exists choosers;
 
 drop table if exists ColorSchemes;
 
+drop table if exists Images;
+
 drop table if exists Members;
 
 drop table if exists Templates;
@@ -84,6 +101,8 @@ SET REFERENTIAL_INTEGRITY TRUE;
 drop sequence if exists choosers_seq;
 
 drop sequence if exists ColorSchemes_seq;
+
+drop sequence if exists Images_seq;
 
 drop sequence if exists Members_seq;
 
