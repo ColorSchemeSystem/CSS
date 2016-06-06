3c9d78c60d66e5a958b4e45341562efbc4e0cd79
masataka.okudera <m.okudera@tcmobile.jp>
2016-06-06 17:35:20 +0900

    a

---
+++ app/models/Image.java
@@ -0,0 +1,25 @@
+package models;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import play.db.ebean.Model.Finder;
+
+@Entity
+@Table(name = "Images")
+public class Image extends BaseModel {
+	@Id
+	public Long imageId;
+	
+	@ManyToOne(cascade = CascadeType.PERSIST)
+	public Member member;
+	
+	public String imageName;
+
+	public String imageMessage;
+	
+	public static Finder<Long, Image> find = new Finder<Long, Image>(Long.class, Image.class);
+}
