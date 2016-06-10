44285a666e1d3a81b55635dd9af99d4a3b9ad412
ryuya.umino <r.umino@tcmobile.jp>
2016-06-10 18:17:13 +0900

    ログイン後以外のデザイン

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -13,26 +13,29 @@ var iframeMethod;
 $(window).load(function(){
 	$('#loading').css("display", "block");
 	$('#loader').css("display", "block");
-	var navi = $('.fixnav');
-		var main  = $('.main');
-		var target_top = navi.offset().top - parseInt(navi.css('margin-top'),10);
-		var sub_top = main.offset().top - parseInt(main.css('margin-top'),10);
-		var sub_scroll = main.offset().top + main.outerHeight(true) - navi.outerHeight(true) - parseInt(navi.css('margin-top'),10);
-
-		if (navi.outerHeight(true) + target_top < main.outerHeight(true) + sub_top) {
-			$(window).scroll(function () {
-				var ws = $(window).scrollTop();
-				$('.scroll').text(ws);
-				if (ws > sub_scroll) {
-				navi.css({position:'fixed', top: sub_scroll - ws + 'px'});
-				} else if(ws > target_top) {
-				navi.css({position:'fixed', top: '0px'});
-				} else {
-				navi.css({position:'relative', top: '0px'});
-				}
-			});
-		}
 });
+
+function fixSideBar(){
+	var navi = $('.fixnav');
+	var main  = $('.main');
+	var target_top = navi.offset().top - parseInt(navi.css('margin-top'),10);
+	var sub_top = main.offset().top - parseInt(main.css('margin-top'),10);
+	var sub_scroll = main.offset().top + main.outerHeight(true) - navi.outerHeight(true) - parseInt(navi.css('margin-top'),10);
+
+	if (navi.outerHeight(true) + target_top < main.outerHeight(true) + sub_top) {
+		$(window).scroll(function () {
+			var ws = $(window).scrollTop();
+			$('.scroll').text(ws);
+			if (ws > sub_scroll) {
+				navi.css({position:'fixed', top: sub_scroll - ws + 'px'});
+			} else if(ws > target_top) {
+				navi.css({position:'fixed', top: '0px'});
+			} else {
+				navi.css({position:'relative', top: '0px'});
+			}
+		});
+	}
+}
 
 function fixFrameSize() {
 	if (window.parent) {
@@ -289,6 +292,7 @@ function reloadIframe(url){
 		$('#afterLoad').append(iframe);
 		$('#afterLoad').css("display", "block");
 		fixFrameSize();
+		fixSideBar()
 	}
 }
 
